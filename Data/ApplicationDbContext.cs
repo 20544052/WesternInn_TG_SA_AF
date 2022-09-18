f0495bdb62b12928e59970fb586f755fff634837
20544052 <113785077+20544052@users.noreply.github.com>
2022-09-18 12:28:41 +1000

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WesternInn_TG_SA_AF.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
