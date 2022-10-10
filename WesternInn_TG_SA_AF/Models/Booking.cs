dc54e6c7fa529beec41bbd349fe4e22fd4e1a12b
UncannyAxeman <19052508@student.westernsydney.edu.au>
2022-10-10 13:46:16 +1100

    Booking model added

--- WesternInn_TG_SA_AF/Models/Booking.cs
+++ WesternInn_TG_SA_AF/Models/Booking.cs
@@ -1,12 +1,32 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
+using System.Drawing.Printing;
 
 namespace WesternInn_TG_SA_AF.Models
 {
     public class Booking
     {
-
+        //Primary Key
         [Key, Required]
         public int Id { get; set; }
+
+        //Foreign Key
+        public int RoomId { get; set; }
+
+        //Foreign Key
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string GuestEmail { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateTime CheckIn { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CheckOut { get; set; }
+
+        [Range(0, 10000.0)]
+        public decimal Cost { get; set; }
+
+        public Room? TheRoom { get; set; }
+        public Guest? TheGuest { get; set; }
     }
 }
